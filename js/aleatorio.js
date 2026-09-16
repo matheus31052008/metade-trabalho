2fb4b3c325c84a0f339948a4cb4c46e3a25da57b
matheus <pereira.matheus31@escola.pr.gov.br>
2026-09-16 13:48:27 -0300

    Update and rename aleatorio.js to aleatorio

--- js/aleatorio.js
+++
@@ -1,9 +0,0 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
-
-export function aleatorio(lista) {
-    const posicao = Math.floor(Math.random() * lista.length);
-    return lista[posicao];
-}
-
-// Exporta o nome sorteado para ser usado no script principal
-export const nome = aleatorio(nomes);
